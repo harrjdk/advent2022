556293fffb7e8224e699471598ab98292f544c40
J Devin Harrison <46249621+harrjdk@users.noreply.github.com>
2022-12-04 17:14:30 -0500

    Day 4
    We're going to introduce a module here, Rebellion's ranges so make this a little easier.
    writing a module to check if one range (a pair in essence) contains another or if they're connected/overlapped is a couple if statements but relying on a more feature mature and excellent module is a practice I'd encourage.

--- day4.rkt
+++ day4.rkt
@@ -1,1 +1,29 @@
 #lang racket
+(require rebellion/base/range)
+(define input (file->string "day4.txt"))
+;; (println input)
+(define inlines (string-split input #rx"\r?\n"))
+;; (println inlines)
+(define inpairs (map (lambda (x)
+                       (let* ([splitstring (string-split x ",")]
+                              [splitnumbers (map (lambda (y) (map string->number (string-split y "-"))) splitstring)]
+                              [ranges (map (lambda (x) (range (inclusive-bound (car x)) (inclusive-bound (cadr x)))) splitnumbers)])
+                         ranges)) inlines))
+;; (println inpairs)
+;; let's assume pairs only for now just like the problem statement
+(define (contains ranges)
+  (let ([a (car ranges)]
+        [b (cadr ranges)])
+    (or (range-encloses? a b) (range-encloses? b a))))
+(define containslist (map contains inpairs))
+;; (println containslist)
+(define totalcontains (apply + (map (lambda (x) (if x 1 0)) containslist)))
+(println totalcontains)
+;; define an overlap check, again rebellion makes this easy
+(define (overlaps ranges)
+  (let ([a (car ranges)]
+        [b (cadr ranges)])
+    (range-connected? a b)))
+(define overlaplist (map overlaps inpairs))
+(define totaloverlap (apply + (map (lambda (x) (if x 1 0)) overlaplist)))
+(println totaloverlap)
